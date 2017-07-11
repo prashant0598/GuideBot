b24ab594b759a9cd311e4685b2253c7726452846
Prashant Nigam <nigamprashant.0598@gmail.com>
2017-07-12 00:23:12 +0530

    Returning response to text messages

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os, sys
 from flask import Flask, request
-
+from pymessenger import Bot
 app = Flask(__name__)
 
+PAGE_ACCESS_TOKEN = "EAAEa7UKKc6YBAEpZAFsrZBFRbtZBlNAznGlGGMhdyZCfgEQx9XhsDfZCKR6ZAIdQ1hFxJmUZC3auCBB78ZBd1GBYEzGFgLbut2NpZAFugQWLZCXGKIUS4n67sqMEqJ9nOQlWG9zWpumrUxjOuTfkxn37Q51AQbSzmcV98NoS8NrDdqTETV5nB81S4J"
 
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -18,6 +20,26 @@ def verify():
 def webhook():
 	data = request.get_json()
 	log(data)
+	
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					# bot
+					response = messaging_text
+					bot.send_text_message(sender_id, response)
+
 
 	return "ok", 200
 def log(message):
