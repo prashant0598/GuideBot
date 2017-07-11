7f438f0e4f71867505c72b791ff96a003d68e614
Prashant Nigam <nigamprashant.0598@gmail.com>
2017-07-12 02:50:03 +0530

    removing errors

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os, sys
 from flask import Flask, request
+from utils import wit_response, get_news_elements
+from pymessenger import Bot
 
 app = Flask(__name__)
 
+PAGE_ACCESS_TOKEN = "fb_page_access_token_here"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 
 @app.route('/', methods=['GET'])
@@ -14,16 +19,38 @@ def verify():
         return request.args["hub.challenge"], 200
     return "Hello world", 200
 
+
 @app.route('/', methods=['POST'])
 def webhook():
 	data = request.get_json()
 	log(data)
 
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					categories = wit_response(messaging_text)
+					elements = get_news_elements(categories)
+					bot.send_generic_message(sender_id, elements)
+
 	return "ok", 200
+
+
 def log(message):
 	print(message)
 	sys.stdout.flush()
-	
+
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 80)
