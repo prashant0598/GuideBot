6be1f636fe1d532d1b59064de23cec19ebf0cadc
Prashant Nigam <nigamprashant.0598@gmail.com>
2017-07-11 22:47:35 +0530

    Creating web hook

---
+++ app.py
@@ -0,0 +1,20 @@
+import os, sys
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def verify():
+	# Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
+
+
+
+if __name__ == "__main__":
+	app.run(debug = True, port = 80)
